7b3b88a82bf4dc0585994602d49f5029f244289f
padmanabh ganorkar <padmanabh_ganorkar@persistent.com>
2021-02-12 11:48:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/main.css";
+import Logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen bg-blue-200 flex justify-center items-center">
+      <div className="rounded-lg shadow-xl flex space-x-2 flex-row p-4 bg-white w-3/4  items-center h-44 justify-center">
+        <div className=" bg-black-200 w-1/5 flex-none">
+          <img src={Logo} />
+        </div>
+        <div className="overflow-auto max-h-full">
+Commodo commodo sit labore consequat deserunt Lorem non cupidatat eiusmod amet. Deserunt id quis labore mollit aliqua veniam qui. Lorem minim culpa officia veniam ut pariatur excepteur minim ipsum magna nostrud amet.
+Commodo commodo sit labore consequat deserunt Lorem non cupidatat eiusmod amet. Deserunt id quis labore mollit aliqua veniam qui. Lorem minim culpa officia veniam ut pariatur excepteur minim ipsum magna nostrud amet.
+Commodo commodo sit labore consequat deserunt Lorem non cupidatat eiusmod amet. Deserunt id quis labore mollit aliqua veniam qui. Lorem minim culpa officia veniam ut pariatur excepteur minim ipsum magna nostrud amet.
+Commodo commodo sit labore consequat deserunt Lorem non cupidatat eiusmod amet. Deserunt id quis labore mollit aliqua veniam qui. Lorem minim culpa officia veniam ut pariatur excepteur minim ipsum magna nostrud amet.
+
+        </div>
+      </div>
     </div>
   );
 }
